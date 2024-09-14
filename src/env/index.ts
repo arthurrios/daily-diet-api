04283452c1b21a1916f7e363233ebd2d2e8ee936
arthur <arthur.rios007@gmail.com>
2024-09-14 15:29:33 -0300

    add users table

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv'
 import { z } from 'zod'
 
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV === 'test') {
   config({ path: '.env/test' })
 } else {
   config()
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV !== 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
-  DATABASE_CLIENT: z.enum(['pg', 'sqlite']),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 })

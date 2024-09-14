a39cae3605e54ab91edb7977af421152362c52ec
arthur <arthur.rios007@gmail.com>
2024-09-14 17:50:34 -0300

    add create meal and get meals routes

---
+++ src/routes/meals.ts
@@ -0,0 +1,42 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '../database'
+import { randomUUID } from 'crypto'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
+
+export async function mealsRoutes(app: FastifyInstance) {
+  app.post(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      const createMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(),
+      })
+
+      const { name, description, isOnDiet, date } = createMealBodySchema.parse(
+        request.body,
+      )
+
+      await knex('meals').insert({
+        id: randomUUID(),
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date,
+        user_id: request.user?.id,
+      })
+
+      return reply.status(201).send()
+    },
+  )
+
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const meals = await knex('meals').where({
+      user_id: request.user?.id,
+    })
+    return { meals }
+  })
+}
